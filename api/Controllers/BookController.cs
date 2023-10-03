cfd50c52f2d44a133d507d6bc8dc8581223a99d4
mariahedkvist <mariasofiahedkvist@gmail.com>
2023-10-03 10:43:32 +0200

    Http-stubbar

--- api/Controllers/BookController.cs
+++ api/Controllers/BookController.cs
@@ -6,5 +6,15 @@ namespace api.Controllers;
 [Route("api/v1/books")]
 public class BookController : ControllerBase
 {
-    
+    [HttpGet("{id}")]
+    public ActionResult GetById(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPost()]
+    public ActionResult Create()
+    {
+        return Created(nameof(GetById), new { message = "Created {id}" });
+    }
 }
