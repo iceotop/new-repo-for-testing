a73bce420f2322c08c16a6eaecd3f7ddce9a7638
Daniel Hertz <112414476+dshertz@users.noreply.github.com>
2023-10-03 10:13:53 +0200

    Kopplat Databasen till modellerna inför migration

--- api/Data/BookCircleContext.cs
+++ api/Data/BookCircleContext.cs
@@ -1,8 +1,12 @@
+using api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data;
 public class BookCircleContext : DbContext
 {
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Event> Events { get; set; }
+    // fixa users
     public BookCircleContext(DbContextOptions options) : base(options)
     {
     }
