9fd8d4b5c59f689681279276a9a2b75c640e4db1
mariahedkvist <mariasofiahedkvist@gmail.com>
2023-10-05 11:54:45 +0200

    Relations + uppdaterade controller-metoder

---
+++ api/ViewModels/EventBaseViewModel.cs
@@ -0,0 +1,10 @@
+namespace api.ViewModels;
+
+public class EventBaseViewModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public ICollection<BookBaseViewModel>? Books { get; set; }
+}
