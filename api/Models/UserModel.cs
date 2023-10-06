d536d18cf47664e55fbfbe3678480e734827a06d
mariahedkvist <mariasofiahedkvist@gmail.com>
2023-10-06 13:53:05 +0200

    Book-user relationer

--- api/Models/UserModel.cs
+++ api/Models/UserModel.cs
@@ -6,6 +6,7 @@ public class UserModel : IdentityUser
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    
+
     public ICollection<Event> Events { get; set; } = new List<Event>();
+    public ICollection<Book> Books { get; set; } = new List<Book>();
 }
