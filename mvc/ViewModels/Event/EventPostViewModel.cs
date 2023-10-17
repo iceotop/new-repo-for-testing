8c5bc1b3fb5f39d055c073bfae2b747e9f72dbe3
Gustav Skogström <gustav.skogstrom@gmail.com>
2023-10-17 14:40:04 +0200

    Views/ViewModel/Model

---
+++ mvc/ViewModels/Event/EventPostViewModel.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.ViewModels.Event
+{
+    public class EventPostViewModel
+    {
+        [Required(ErrorMessage = "Title är obligatoriskt")]
+        [DisplayName("Title")]
+        public string Title {get; set;} = "";
+
+        [Required(ErrorMessage = "Book är obligatoriskt")]
+        [DisplayName("Book")]
+        public string Book {get; set;} = "";
+
+        [Required(ErrorMessage = "Description är obligatoriskt")]
+        [DisplayName("Description")]
+        public string Description {get; set;} = "";
+
+        [Required(ErrorMessage = "StartDate är obligatoriskt")]
+        [DisplayName("StartDate")]
+        public string StartDate {get; set;} = "";
+
+        [Required(ErrorMessage = "EndDate är obligatoriskt")]
+        [DisplayName("EndDate")]
+        public string EndDate {get; set;} = "";
+        
+    }
+}
