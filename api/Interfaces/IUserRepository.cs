fdd950b459ab0d88b5839ae478cadb71be1c18bf
Daniel Hertz <112414476+dshertz@users.noreply.github.com>
2023-10-27 11:44:55 +0200

    Lagt till AddAsync i IUserRepository

--- api/Interfaces/IUserRepository.cs
+++ api/Interfaces/IUserRepository.cs
@@ -7,6 +7,7 @@ public interface IUserRepository
     Task<UserModel?> FindByIdAsync(string id);
     Task<UserModel?> FindByEmailAsync(string email);
     // Task<bool> UpdateAsync(UserModel user); - Kanske behövs sen?
+    Task<bool> AddAsync(UserModel user);
     Task<bool> DeleteAsync(UserModel user);
     Task<bool> SaveAsync();
 }
