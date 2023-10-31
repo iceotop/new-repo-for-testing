2581b28f79868a95b9aa0008225291ed1f7b7458
Daniel Hertz <112414476+dshertz@users.noreply.github.com>
2023-10-31 17:04:08 +0100

    EventPostViewModel

---
+++ api/ViewModels/EventPostViewModel.cs
@@ -0,0 +1,9 @@
+namespace api.ViewModels;
+public class EventPostViewModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Book { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
